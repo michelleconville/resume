a5d37379b56ea080e3f7aca5bac6fa2cd3dd5127
michelleconville <michelle.conville@gmail.com>
2023-03-11 23:14:11 +0000

    Implement emailJS onthe contact page

---
+++ assets/js/sendEmail.js
@@ -0,0 +1,32 @@
+/*function sendMail(contactForm) {
+    emailjs.send("service_gh1cukf","rosie-2023-03-11", {
+        "from_name": contactForm.name.value,
+        "from_email": contactForm.emailaddress.value,
+        "project_request": contactForm.projectsummary.value
+    })
+    .then(
+        function(response) {
+            console.log("SUCCESS", response);
+        },
+        function(error) {
+            console.log("FAILED", error);
+        }
+    );
+    return false;  // To block from loading a new page
+}
+*/
+
+
+
+emailjs.sendForm("service_gh1cukf","rosie-2023-03-11", 'form')
+    .then(function(response) {
+       console.log('SUCCESS!', response.status, response.text);
+    }, function(error) {
+       console.log('FAILED...', error);
+    });
+
+
+
+  
+
+    
