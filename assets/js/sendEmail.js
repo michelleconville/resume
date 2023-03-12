d65fa3b1d53675f5e2331518062878f671f50dbd
michelleconville <michelle.conville@gmail.com>
2023-03-12 19:06:41 +0000

    Fix bug in emailJS

--- assets/js/sendEmail.js
+++ assets/js/sendEmail.js
@@ -1,7 +1,7 @@
-/*function sendMail(contactForm) {
-    emailjs.send("service_gh1cukf","rosie-2023-03-11", {
+ function sendMail(contactForm) {
+    emailjs.send('service_gh1cukf', 'rosie-2023-03-11', {
         "from_name": contactForm.name.value,
-        "from_email": contactForm.emailaddress.value,
+        "from_emailaddress": contactForm.emailaddress.value,
         "project_request": contactForm.projectsummary.value
     })
     .then(
@@ -14,19 +14,3 @@
     );
     return false;  // To block from loading a new page
 }
-*/
-
-
-
-emailjs.sendForm("service_gh1cukf","rosie-2023-03-11", 'form')
-    .then(function(response) {
-       console.log('SUCCESS!', response.status, response.text);
-    }, function(error) {
-       console.log('FAILED...', error);
-    });
-
-
-
-  
-
-    
